ca2a2a1be9faef18e2d888eb21c10179c409cbc4
Daniel Lemos <daniel.lemos@yetz.com.br>
2025-09-04 16:56:02 -0300

    GP04: Implementação da autenticação com Laravel Sanctum
    - Implementação de middleware de autenticação
    - Configuração de CORS para desenvolvimento
    - Otimização de hash com Argon2ID
    - Configuração de debugging com Xdebug

--- routes/api.php
+++ routes/api.php
@@ -1,8 +1,24 @@
 <?php
 
+use App\Models\User;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use Illuminate\Support\Facades\Auth;
 
 Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
+
+Route::post('/tokens/create', function (Request $request) {
+    // Forçar debug aqui
+
+    //$user = User::where('email', $request->email);
+
+    $auth = Auth::attempt(['email' => $request->email, 'password' => $request->password]);
+
+    $userToken = $request->user()->createToken('token-api');
+
+    return response()->json([
+        'access_token' => $userToken->plainTextToken,
+    ]);
+});
